6706919caf1ab60e87462f37e11dd62f2807d7b4
ssline <778752178@qq.com>
2026-05-18 23:12:06 +0800

    feat: create project and first commit

---
+++ src/main/java/com/example/githubactionsdemo/GithubActionsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.githubactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsDemoApplication.class, args);
+        System.out.println("GithubActionsDemoApplication Start....");
+    }
+
+}
