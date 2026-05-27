06ce8de8d5da86b964028f414c821ab9f36ffdb7
ssline <778752178@qq.com>
2026-05-27 23:28:01 +0800

    refactor: remove unnecessary constructor and javadoc

--- src/main/java/com/example/githubactionsdemo/GithubActionsDemoApplication.java
+++ src/main/java/com/example/githubactionsdemo/GithubActionsDemoApplication.java
@@ -6,14 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubActionsDemoApplication {
 
-    protected GithubActionsDemoApplication() {
-    }
-
-    /**
-     * Application entry point.
-     *
-     * @param args command line arguments
-     */
     public static void main(final String[] args) {
         SpringApplication.run(GithubActionsDemoApplication.class, args);
         System.out.println("GithubActionsDemoApplication Start....");
